d46b778237baee1bc8c0805dcc1e9480c080e3c3
heo <admin@admin.com>
2025-02-25 13:16:20 +0900

    commit

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
-const Hello = () => {
-  return <div>안녕하세요</div>
+//const Hello = (props) => {
+const Hello = ({ name, age, color }) => {
+  // props ={
+  //   name:"홍길동",
+  //    age:"29",
+  //     color:"red"
+  // }
+  return (
+    //prettier-ignore
+    <div style={{ color: color}}>
+      안녕하세요:{name} <br/>
+      색상:{color}<br/>
+      나이:{age}<br/>
+    </div>
+  )
 }
 
 export default Hello

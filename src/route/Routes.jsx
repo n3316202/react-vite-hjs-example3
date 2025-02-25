c8bd09d5028a3a06d1d9e5ccedbfbeb7e87f4eaf
heo <admin@admin.com>
2025-02-25 16:04:23 +0900

    commit

--- src/route/Routes.jsx
+++ src/route/Routes.jsx
@@ -8,6 +8,7 @@ import JSXPage from '../pages/JSXPage'
 import ProfilePage from '../pages/ProfilePage'
 import PropsPage from '../pages/PropsPage'
 import UseEffectPage from '../pages/UseEffectPage'
+import UseRef1Page from '../pages/UseRef1Page'
 
 //React Routes, Route 사용시 배열로 관리하기
 const routes = [
@@ -60,6 +61,11 @@ const routes = [
     path: '/useeffect',
     element: <UseEffectPage />,
     title: 'useEffect-연습',
+  },
+  {
+    path: '/useref',
+    element: <UseRef1Page />,
+    title: 'useRef-1',
   },
 ]
 export default routes
